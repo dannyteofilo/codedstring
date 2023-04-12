a94ecb6f1b0109a68764a175a960a524dd35fe6a
dannyteofilo <danyel.285309@gmail.com>
2023-04-11 22:47:09 -0600

    fix: adding port default

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
