2267dcae1c9a308f49cfc59757d086886c9c4874
dannyteofilo <danyel.285309@gmail.com>
2023-04-11 21:32:12 -0600

    feat: initial config project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import path from "path";
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
